bf39da808c655ef0cf8c0a9350edf43a3b3ca9b9
Fleexie <jannickolsendk@hotmail.com>
2020-11-27 14:21:10 +0100

    Initial commit/push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+//import { IndexComponent } from './index/index.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { HeaderComponent } from './header/header.component';
+//import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+//    IndexComponent,
+//    HeaderComponent,
+//    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
