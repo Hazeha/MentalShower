b9b33cad2c54031bac40fdbb74282bdc64c2a90f
Fleexie <jannickolsendk@hotmail.com>
2020-11-30 13:12:04 +0100

    Different imports Fontawesome slight styling
    few changes here and there

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,50 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-//import { IndexComponent } from './index/index.component';
+import { IndexComponent } from './index/index.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { HeaderComponent } from './header/header.component';
-//import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { routing } from './app-routing';
+
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import {MatCardModule} from '@angular/material/card';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ControlpanelComponent } from './controlpanel/controlpanel.component';
+import { FanContainerComponent } from './fan-container/fan-container.component';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import { FanBasicComponent } from './fan-basic/fan-basic.component';
+import { FanAdvancedComponent } from './fan-advanced/fan-advanced.component';
+import {MatSliderModule} from "@angular/material/slider";
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-//    IndexComponent,
-//    HeaderComponent,
-//    NavComponent
+    IndexComponent,
+    HeaderComponent,
+    NavComponent,
+    ControlpanelComponent,
+    FanContainerComponent,
+    FanBasicComponent,
+    FanAdvancedComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+      routing,
+      MatToolbarModule,
+      MatButtonModule,
+      MatCardModule,
+      FontAwesomeModule,
+      MatStepperModule,
+      MatTabsModule,
+      MatFormFieldModule,
+      MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
