f9bcf52e47c78b0a0c3bdd3c00ca7970a4e65b14
Deepak Gautam <anin0543@edu.easj.dk>
2020-11-30 14:40:50 +0100

    clean file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { AppComponent } from './app.component';
-import {MaterialModule} from '../material/material.module';
-
+import { MaterialModule } from '../material/material.module';
 import { routing } from './app-routing';
-import { AppRoutingModule } from './app.routing';
-
-
-import { IndexComponent } from './index/index.component';
-import { HeaderComponent } from './header/header.component';
-import { NavComponent } from './nav/nav.component';
-import { HomeComponent } from './home/home.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { LearningElementComponent } from './learning-element/learning-element.component';
+import { AppComponent } from './app.component';
 import { ControlpanelComponent } from './controlpanel/controlpanel.component';
-import { FanContainerComponent } from './fan-container/fan-container.component';
-import { FanBasicComponent } from './fan-basic/fan-basic.component';
 import { FanAdvancedComponent } from './fan-advanced/fan-advanced.component';
-
+import { FanBasicComponent } from './fan-basic/fan-basic.component';
+import { FanContainerComponent } from './fan-container/fan-container.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { IndexComponent } from './index/index.component';
+import { LearningElementComponent } from './learning-element/learning-element.component';
+import { NavComponent } from './nav/nav.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
-
-
 
 @NgModule({
   declarations: [
@@ -43,12 +34,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-      routing,
-      FontAwesomeModule,
+    routing,
+    FontAwesomeModule,
     MaterialModule,
     // AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
